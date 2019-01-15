7c9f73944b3476f383d8ccd36a2cab7974532387
PineGolden311 <software.developer112@gmail.com>
2019-01-16 01:15:47 +0400

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   
-  get '/test', to: 'pusher_test#test'
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :show, :destroy]
+  end
 end
--- test/controllers/pusher_test_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PusherTestControllerTest < ActionDispatch::IntegrationTest
-  test "should get test" do
-    get pusher_test_test_url
-    assert_response :success
-  end
-
-end

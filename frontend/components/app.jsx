05766273e4e7e01d01bdfd2b663d67ccebffdb25
PineGolden311 <software.developer112@gmail.com>
2019-02-26 00:30:50 +0400

    Styled Authentication Form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,18 +12,18 @@ const App = ({ children }) => (
       </div>
     </nav>
     
-    {/* <div className='homepage-hero-module'>
+    <div className='homepage-hero-module'>
       <div className='video-container'>
         <div className='filter'></div>
         <video autoPlay loop className='fillWidth'>
           <source src={ window.assets.landingPageVideoMP4 } type="video/mp4" />
           <source src={ window.assets.landingPageVideoWEBM } type="video/webm" />
         </video>
-        <div className='poster hidden'>
-          <img src={ window.assets.landingPageImage } alt="" />
-        </div>
       </div>
-    </div> */}
+    </div>
+     {/* <div className="poster hidden">
+       <img src={ window.assets.landingPageImage } alt="" />
+     </div> */}
     <section className='session-form-container'>
       { children }
     </section>
